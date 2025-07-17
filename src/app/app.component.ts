a4dc11db3d3b0da3618617bdc98a7a3c1bfa3ca1
bebicknobel <bebicknobel2000@gmail.com>
2025-07-17 11:08:05 +0530

    Typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my_FirstApp';
+
+
+  constructor() {}
+
+  name:string ='';
+
+  countval=0
+  countInc(){
+  
+   this.countval++;
+  }
+
+  countDec(){
+    if(this.countval <=0 ){     
+      this.countval;
+    }
+    else{
+      this.countval--;
+    }
+    
+  }
+
+   fruits= ['Apple','Orange','Mango','PineApple'];
+
+  showMe=false;
+
+  applyColor=false;
 }
