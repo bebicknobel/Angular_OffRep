e56aa5649cfc0aca9bda55c74ae66202be8c4d33
bebicknobel <bebicknobel2000@gmail.com>
2025-07-17 12:03:41 +0530

    Custom Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule,CommonModule],
+  imports: [RouterOutlet,FormsModule,CommonModule,HighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
